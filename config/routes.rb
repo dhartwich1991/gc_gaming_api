b0ee26d2ae5ac117234f1b4eee6e8f2d71ebb53c
Daniel Hartwich <daniel.hartwich@xing.com>
2014-12-12 16:17:13 +0100

    change registration to post

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
 	    resources :users do
         collection do
-          get :register
+          post 'register'
         end
       end
       resources :raids
