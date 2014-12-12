caf75a0e47f0f282aaa2c3233b3dee5c04e5c6eb
Daniel Hartwich <daniel.hartwich@xing.com>
2014-12-12 14:29:42 +0100

    fox users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources:raids
+  resources :raids
 	namespace :api do
 	  namespace :v1 do
-	    resources:users
+	    resources :users
 			
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
