fd96838627c33a825d1cd8e75011421431351b8e
Daniel Hartwich <daniel.hartwich@xing.com>
2014-12-12 15:47:38 +0100

    add json response on success and error

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,40 +1,40 @@
-module Api
-	module V1
-		class UsersController < ApplicationController
-			def index
-				render json: {message: 'UsersController'}
-				db = SQLite3::Database.open "user.db"
-				db.execute "CREATE TABLE IF NOT EXISTS User(Id INTEGER PRIMARY KEY AUTOINCREMENT, 
-        Name TEXT, Password TEXT)"
-				db.execute "INSERT INTO User (Name,Password) VALUES(#{:name},#{:password})"
-				rescue SQLite3::Exception => e 
-    
-    				puts "Exception occurred"
-    				puts e
-    
-				ensure
-    				db.close if db
-			end
-			def register
-				name = params[:username]
-				password = params[:password]
-				db = SQLite3::Database.open "user.db"
-				db.execute "CREATE TABLE IF NOT EXISTS User(Id INTEGER PRIMARY KEY AUTOINCREMENT, 
-        Name TEXT, Password TEXT)"
-				db.execute "INSERT INTO User (Name,Password) VALUES(#{name},#{password})"
-				rescue SQLite3::Exception => e 
-    
-    				puts "Exception occurred"
-    				puts e
-    
-				ensure
-    				db.close if db
-			end
-			def login
-			end
-			def list_friends(user_id)
-				
-			end
-		end
-	end
+	module Api
+		module V1
+			class UsersController < ApplicationController
+				def index
+					render json: {message: 'UsersController'}
+					db = SQLite3::Database.open "user.db"
+					db.execute "CREATE TABLE IF NOT EXISTS User(Id INTEGER PRIMARY KEY AUTOINCREMENT, 
+						Name TEXT, Password TEXT)"
+	db.execute "INSERT INTO User (Name,Password) VALUES(#{:name},#{:password})"
+rescue SQLite3::Exception => e 
+	
+	puts "Exception occurred"
+	puts e
+	
+ensure
+	db.close if db
+end
+def register
+	name = params[:username]
+	password = params[:password]
+	db = SQLite3::Database.open "user.db"
+	db.execute "CREATE TABLE IF NOT EXISTS User(Id INTEGER PRIMARY KEY AUTOINCREMENT, 
+		Name TEXT, Password TEXT)"
+	db.execute "INSERT INTO User (Name,Password) VALUES(#{name},#{password})"
+	render json {message: 'sucess'}
+rescue SQLite3::Exception => e 
+	
+	render json {message: 'error'}
+	
+ensure
+	db.close if db
+end
+def login
+end
+def list_friends(user_id)
+	
+end
+end
+end
 end
