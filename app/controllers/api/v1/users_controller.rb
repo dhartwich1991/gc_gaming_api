fcd123d287f386253791195c1b389284cb74462d
Daniel Hartwich <daniel.hartwich@xing.com>
2014-12-12 14:30:54 +0100

    add end to users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,7 @@ module Api
 	module V1
 		class UsersController < ApplicationController
 			def index
-		render json: {message: 'Hello Worlde'}
+			render json: {message: 'Hello Worlde'}
+		end
 	end
 end
