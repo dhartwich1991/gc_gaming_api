f33bb80419cbea3f6ecf2b385a51ba5e72d2cd0d
Daniel Hartwich <daniel.hartwich@xing.com>
2014-12-12 14:00:37 +0100

    adapt routes to work properly

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources: raids
 	namespace :api do
 	  namespace :v1 do
-	    resources:user
+	    resources:users
 			
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
