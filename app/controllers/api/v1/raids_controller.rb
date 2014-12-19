efc41f1ac75507d1071842c250371dcc053d374e
Daniel Hartwich <daniel.hartwich@xing.com>
2014-12-19 13:33:12 +0100

    start to work on raids controller

--- app/controllers/api/v1/raids_controller.rb
+++ app/controllers/api/v1/raids_controller.rb
@@ -2,8 +2,10 @@ module Api
 	module V1
 		class RaidsController < ApplicationController
 			def index
-				#this is the default call and should list all raids.
-				render json: {message: "Raids"}
+				#This is the default call and should list all raids.
+				@raids = Raid.all
+				respond_to do |format|
+					format.json { render :json => @raids }
 			end
 		end
 	end
