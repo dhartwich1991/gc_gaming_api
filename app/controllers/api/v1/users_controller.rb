23db194ff3e680660fd1f3e1dbc808deae0ee8f1
Daniel Hartwich <daniel.hartwich@xing.com>
2014-12-19 10:12:45 +0100

    formatting in userscontroller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,24 +1,24 @@
-	module Api
-		module V1
-			class UsersController < ApplicationController
-	def index
-		@users = User.all
-		respond_to do |format|
-			format.json { render :json => @users }
+module Api
+	module V1
+		class UsersController < ApplicationController
+			def index
+				@users = User.all
+				respond_to do |format|
+					format.json { render :json => @users }
+			end
+			def register
+				username = params[:username]
+				password = params[:password]
+
+			end
+			def login
+				username = params[:username]
+				password = params[:password]
+			end
+			def list_friends
+
+			end
+
+		end
 	end
-def register
-	username = params[:username]
-	password = params[:password]
-
-end
-def login
-	username = params[:username]
-	password = params[:password]
-end
-def list_friends()
-
-end
-
-end
-end
 end
