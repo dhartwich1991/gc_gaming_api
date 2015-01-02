f82b6976c570f93b9a41af750eb999a324dfd2e1
Daniel Hartwich <daniel.hartwich@xing.com>
2015-01-02 16:41:27 +0100

    modify migration

--- db/migrate/20150102153743_create_users_raids.rb
+++ db/migrate/20150102153743_create_users_raids.rb
@@ -1,6 +1,10 @@
 class CreateUsersRaids < ActiveRecord::Migration
   def change
-    create_table :users_raids do |t|
+    create_table :users_raids, :id => false do |t|
+    	t.references :user
+    	t.references :raid
     end
+    add_index :users_raids, [:user_id, :raid_id]
+    add_index :users_raids, :user_id
   end
 end
