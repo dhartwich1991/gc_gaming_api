52132823e6064ad0934dd3ca7be1eca59dd2bf37
Daniel Hartwich <daniel_hartwich@icloud.com>
2015-01-02 16:30:00 +0100

    new migration for user raid relation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150102090425) do
+ActiveRecord::Schema.define(version: 20150102152806) do
 
   create_table "raids", force: true do |t|
     t.string   "name"
