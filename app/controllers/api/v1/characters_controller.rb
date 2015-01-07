7bcd29861502191ca9a92c7b40e69ce5f7b97eb3
Daniel Hartwich <daniel.hartwich@xing.com>
2015-01-07 16:15:29 +0100

    set values according to type

--- app/controllers/api/v1/characters_controller.rb
+++ app/controllers/api/v1/characters_controller.rb
@@ -10,19 +10,19 @@ module Api
 			def create
 				@newchar = Character.new
 
-				@newchar.lastModified = params[:lastModified]
+				@newchar.lastModified = params[:lastModified].to_i
 				@newchar.name = params[:name]
 				@newchar.realm = params[:realm]
 				@newchar.battlegroup = params[:battlegroup]
-				@newchar.class = params[:class]
-				@newchar.race = params[:race]
-				@newchar.gender = params[:gender]
-				@newchar.level = params[:level]
-				@newchar.achievementPoints = params[:achievementPoints]
+				@newchar.class = params[:class].to_i
+				@newchar.race = params[:race].to_i
+				@newchar.gender = params[:gender].to_i
+				@newchar.level = params[:level].to_i
+				@newchar.achievementPoints = params[:achievementPoints].to_i
 				@newchar.thumbnailurl = params[:thumbnailurl]
-				@newchar.itemleveltotal = params[:itemleveltotal]
-				@newchar.itemlevelequipped = params[:itemlevelequipped]
-				@newchar.userid = params[:userid]
+				@newchar.itemleveltotal = params[:itemleveltotal].to_i
+				@newchar.itemlevelequipped = params[:itemlevelequipped].to_i
+				@newchar.userid = params[:userid].to_i
 
 				puts newchar
 
