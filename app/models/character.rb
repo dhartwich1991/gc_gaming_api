4ef830dd3dff3afcc43102f2a3bc1419cfd4e53a
Daniel Hartwich <daniel.hartwich@xing.com>
2015-01-09 09:51:24 +0100

    switch relation

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,5 +1,6 @@
 class Character < ActiveRecord::Base
 	belongs_to :user
 	has_one :user
+	
 	validates_uniqueness_of :thumbnailurl, scope: :user_id
 end
--- app/models/users_raids.rb
+++ app/models/users_raids.rb
@@ -1,6 +1,4 @@
 class UsersRaids < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :raid
   has_one :raid
   has_one :user
   has_one :character
